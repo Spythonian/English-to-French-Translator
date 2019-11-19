db320403c1b6e570a45c3ad9b9a7b2d75adb9b60
spythonian <spythonian@gmail.com>
2019-11-19 18:52:54 +0100

    Added Speech Reference

--- English to French Translator/TranslatePage.cs
+++ English to French Translator/TranslatePage.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Speech.Recognition;
+using System.Threading;
 
 namespace English_to_French_Translator
 {
@@ -15,12 +18,26 @@ namespace English_to_French_Translator
             InitializeComponent();
         }
 
+        // Implementing the Speech Reference
+        SpeechSynthesizer sSynth = new SpeechSynthesizer();
+        PromptBuilder pBuilder = new PromptBuilder();
+        SpeechRecognitionEngine sRecognize = new SpeechRecognitionEngine();
         private void Button4_Click(object sender, EventArgs e)
         {
             ChoiceFeed tp = new ChoiceFeed();
             tp.Show();
             this.Hide();
             tp.Focus();
+        }
+
+        private void TranslatePage_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void Button1_Click(object sender, EventArgs e)
+        {
+           
         }
     }
 }
